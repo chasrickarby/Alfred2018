f1efa7961dff4ab6dbbcae544cc736565d34c872
Mark Westover <mark.westover33@gmail.com>
2018-02-05 16:54:56 -0500

    Updates to the Unity project to add a debug scene.

--- Alfred/Assets/Scripts/ExampleClass.cs
+++ Alfred/Assets/Scripts/ExampleClass.cs
@@ -6,21 +6,25 @@ using System;
 
 public class ExampleClass : MonoBehaviour
 {
-    public string url = "http://localhost:51484/api/rooms/POR-cr6@ptc.com";
-    public Text Organizer;
+    public string Url = "http://10.64.105.101:51484/api/rooms/POR-cr6@ptc.com";
+    public Text OrganizerText;
     public Text StartText;
-    public Text End;
+    public Text EndText;
 
     IEnumerator Start()
     {
-        using (WWW www = new WWW(url))
+        using (WWW www = new WWW(Url))
         {
             yield return www;
+            Debug.Log(www.text);
+            Debug.Log(www.error);
             var roomDetails = Room.CreateFromJSON(www.text);
-            DateTime startTime = DateTime.Parse(roomDetails.Events[0].Start);
-            Organizer.text = roomDetails.Events[0].Subject;
-            End.text = roomDetails.Events[0].End;
-
-        }
+            Debug.Log(roomDetails.Events.Length);
+            var startTime = DateTime.Parse(roomDetails.Events[0].Start);
+            var endTime = DateTime.Parse(roomDetails.Events[0].End);
+            OrganizerText.text = roomDetails.Events[0].Subject;
+            StartText.text = $"{startTime:hh:mm tt}";
+            EndText.text = $"{endTime:hh:mm tt}";
+            }
     }
 }
