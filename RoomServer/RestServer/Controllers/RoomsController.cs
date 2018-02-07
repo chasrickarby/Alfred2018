4c29d396ca37461e7ddbec16b31a3b219e35bfb0
Mark Westover <mark.westover33@Gmail.com>
2018-02-07 13:39:07 -0500

    Removed unecessary json conversion from Rest Server and fixed unity project.

--- RoomServer/RestServer/Controllers/RoomsController.cs
+++ RoomServer/RestServer/Controllers/RoomsController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using RoomManager;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace RestServer.Controllers
 {
@@ -22,11 +23,10 @@ namespace RestServer.Controllers
 
         // GET /RestServer/api/rooms?id=POR-cr6@ptc.com
         // GET /RestServer/api/rooms/POR-cr6
-        public string GetAppointmentsByRoomAddress(string id)
+        public Room GetAppointmentsByRoomAddress(string id)
         {
             Room room = exchange.GetAppointmentsByRoomAddress(id, DateTime.Today, DateTime.Today.AddDays(1));
-            string json = JsonConvert.SerializeObject(room);
-            return json;
+            return room;
         }
 
         // POST /RestServer/api/rooms/CreateMeeting/?roomAddress=POR-cr6&subject=blah blah&start=2018-02-06T00:00:00&end=2018-02-07T00:00:00
