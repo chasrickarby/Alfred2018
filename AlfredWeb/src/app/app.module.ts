39e700d1691ea8338f17069361a79d541bf501c9
PTCNET\crickarby <crickarby@ptc.com>
2018-02-09 08:14:27 -0500

    Cleaned up some includes in the calendar-vew and the app.module

--- AlfredWeb/src/app/app.module.ts
+++ AlfredWeb/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { MatList, MatListItem, MatListModule } from '@angular/material'
-
-
+import { MatListModule } from '@angular/material'
 import { AppComponent } from './app.component';
 import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
-
 
 @NgModule({
   declarations: [
