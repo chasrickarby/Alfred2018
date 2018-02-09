04de32341c2cc3bcdf98d9e628261d6b26718452
Sergey Zolotykh <szolotykh88@gmail.com>
2018-02-09 11:12:16 -0500

    Added scheadule view component

--- AlfredWeb/src/app/app.module.ts
+++ AlfredWeb/src/app/app.module.ts
@@ -2,21 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { MatList, MatListItem, MatListModule } from '@angular/material'
-
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { AppComponent } from './app.component';
 import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
+import { ScheduleViewComponent } from './components/schedule-view/schedule-view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarViewComponent
+    CalendarViewComponent,
+    ScheduleViewComponent
   ],
   imports: [
     HttpModule,
     BrowserModule,
-    MatListModule
+    MatListModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
